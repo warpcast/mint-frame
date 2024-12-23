aa489dc6337a97d3790c2fb59156c3d3444fc37d
horsefacts <webmaster@terminally.online>
2024-12-22 21:49:45 -0500

    move opengraph to cloudflare

--- opengraph/functions/[[path]].tsx
+++ opengraph/functions/[[path]].tsx
@@ -1,38 +1,55 @@
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "@vercel/og";
+import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
+import type { EventContext } from "@cloudflare/workers-types";
 
-import { api } from "@/lib/api";
+interface Env {
+  API_URL: string;
+  PUBLIC_URL: string;
+}
 
-export const runtime = "edge";
+type ApiFeaturedMint = {
+  name: string;
+  imageUrl: string;
+  description?: string;
+  creator: {
+    fid: number;
+    username?: string;
+    displayName: string;
+    pfp?: {
+      url: string;
+    };
+  };
+  isMinting: boolean;
+};
 
-export async function GET() {
+export const onRequest = async (
+  context: EventContext<Env, string, unknown>
+) => {
   try {
-    const {
-      data: {
+    const [
+      {
         result: { mint },
       },
-    } = await api.getFeaturedMint();
-
-    const interRegular = await fetch(
-      new URL(
-        "https://wc-featured-mint.vercel.app/fonts/Inter-Regular.ttf",
-        import.meta.url
-      )
-    ).then((res) => res.arrayBuffer());
-
-    const interMedium = await fetch(
-      new URL(
-        "https://wc-featured-mint.vercel.app/fonts/Inter-Medium.ttf",
-        import.meta.url
-      )
-    ).then((res) => res.arrayBuffer());
-
-    const interSemiBold = await fetch(
-      new URL(
-        "https://wc-featured-mint.vercel.app/fonts/Inter-SemiBold.ttf",
-        import.meta.url
-      )
-    ).then((res) => res.arrayBuffer());
+      interRegular,
+      interMedium,
+      interSemiBold,
+    ] = await Promise.all([
+      fetch(`${context.env.API_URL}/v1/featured-mint`).then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch mint data");
+        }
+        return response.json();
+      }) as Promise<{ result: { mint: ApiFeaturedMint } }>,
+      fetch(new URL(`${context.env.PUBLIC_URL}/fonts/Inter-Regular.ttf`)).then(
+        (res) => res.arrayBuffer()
+      ),
+      fetch(new URL(`${context.env.PUBLIC_URL}/fonts/Inter-Medium.ttf`)).then(
+        (res) => res.arrayBuffer()
+      ),
+      fetch(new URL(`${context.env.PUBLIC_URL}/fonts/Inter-SemiBold.ttf`)).then(
+        (res) => res.arrayBuffer()
+      ),
+    ]);
 
     const imageUrl = mint.imageUrl.replace("?width=250", "?width=1600");
 
@@ -196,9 +213,8 @@ export async function GET() {
                   {mint.creator.username}
                 </span>
                 <span>on</span>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
-                  src="https://wc-featured-mint.vercel.app/base-logo.png"
+                  src={`${context.env.PUBLIC_URL}/base-logo.png`}
                   alt="Base"
                   width="48"
                   height="48"
@@ -271,4 +287,4 @@ export async function GET() {
       },
     });
   }
-}
+};
